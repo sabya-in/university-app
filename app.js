e4e64caa7c7a7e4a46278f6b08473230a4a789a2
= <=>
2021-11-04 03:46:22 +0530

    Adding working jwt based authentication

---
+++ app.js
@@ -0,0 +1,59 @@
+/****** Requires *****/
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const session = require("express-session");
+
+// Initialize app with express
+const app = express();
+
+
+/***** MongoDB Connection *****/
+
+const config = require('./config/db');
+mongoose.connect(config.database);
+// On Connection
+mongoose.connection.on('connected', () => {
+   console.log(`connected to database ${config.database}`);
+});
+// On Error
+mongoose.connection.on('error', (err) => {
+   console.log(`Database error: ${err}`);
+});
+
+
+/***** Middleware *****/
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({secret: config.secret}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/pass')(passport);
+
+// Routes
+const users = require('./routes/users');
+app.use('/users', users);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index route
+app.get('/', (req, res) => res.send('Invalid endpoint'));
+
+// Redirect invalid routes
+app.get('*', (req, res) => {
+   //res.sendFile(path.join(__dirname, 'public/index.html'));
+   res.send('Invalid endpoint should redirect to Home')
+});
+
+// Server port
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ routes/users.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+const User = require('../models/users');
+const config = require("../config/db");
+
+// Register
+router.post('/register', (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({success: false, msg: 'Failed to register user'});
+        } else {
+            res.json({success: true, msg: 'User registered'});
+        }
+    });
+});
+
+// Login
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        // There is no user with that username
+        if (!user) {
+            return res.json({success: false, msg: 'User not found'});
+        }
+        // User found, check password
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if(isMatch) {
+
+                // create jwt
+                const token = jwt.sign(user.toObject(), config.secret, {
+                    expiresIn: 900 // 10 min
+                });
+
+                jwt.verify(token, config.secret, (err, data) => {console.log(err, data);});
+
+                res.json({
+                    success: true,
+                    token: `Bearer ${token}`,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({success: false, msg: 'Wrong password'});
+            }
+        });
+    });
+});
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({user: req.user});
+});
+
+module.exports = router;
