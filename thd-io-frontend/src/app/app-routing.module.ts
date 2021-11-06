11410c5e0ac5ec71999b00a91a7b96851c92a309
Vikas Gunti <vikas.gunti@th-deg.de>
2021-11-06 14:07:53 +0530

    Adding Nav toolbar with events,news,navigation components

---
+++ thd-io-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NewsComponent } from './news/news.component';
+
+const routes: Routes = [
+  {path:'news',component : NewsComponent},
+  {path:'events',component : EventsComponent},
+  {path:'navigation',component : NavigationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [NewsComponent, EventsComponent, NavigationComponent]
